bf6df39afc2fcd0ae0b10ad3428cfc8f22607533
andrewRichardson <andyandy698@gmail.com>
2022-11-05 12:18:54 -0500

    Mock out hook to fix test

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -2,8 +2,12 @@ import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
 
-jest.mock("./hooks/useMediaQuery", () => {
+jest.mock("../hooks/useMediaQuery", () => {
     return jest.fn((_: string) => false);
+});
+
+jest.mock("../hooks/useIntersection", () => {
+    return jest.fn(() => false);
 });
 
 test("renders Andrew Richardson", () => {
