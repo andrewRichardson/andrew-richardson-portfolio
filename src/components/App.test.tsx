f07d93500dd5ef51960e0a696fb541b6fcbdf4a6
andrewRichardson <andyandy698@gmail.com>
2023-08-09 01:26:45 -0500

    Remove import

--- src/components/App.test.tsx
+++ src/components/App.test.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { render, screen } from "@testing-library/react";
 import App from "./App";
-import { Entry } from "contentful";
 
 jest.mock("../hooks/useMediaQuery", () => {
     return jest.fn((_: string) => false);
